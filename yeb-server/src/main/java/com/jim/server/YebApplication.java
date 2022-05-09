9df8e6e4b5f249d9790249d84afd88b969be9f53
Jimecc <1813085460@qq.com>
2022-05-09 16:45:36 +0800

    Init Submit

--- yeb-server/src/main/java/com/jim/server/YebApplication.java
+++ yeb-server/src/main/java/com/jim/server/YebApplication.java
@@ -4,9 +4,11 @@ package com.jim.server;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @MapperScan("com.jim.server.mapper")
+@EnableWebMvc
 public class YebApplication {
 
     public static void main(String[] args){
