e7b9834ab726fb92c6a3ab21f92d7a93e62a1fac
Jimecc <1813085460@qq.com>
2022-05-24 23:53:11 +0800

    MailLog_NotDONE

--- yeb-mail/src/main/java/com/jim/mail/mail/MailReceiver.java
+++ yeb-mail/src/main/java/com/jim/mail/mail/MailReceiver.java
@@ -1,6 +1,7 @@
 package com.jim.mail.mail;
 
 import com.jim.server.pojo.Employee;
+import com.jim.server.pojo.MailConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -34,7 +35,7 @@ public class MailReceiver {
     @Autowired
     private TemplateEngine templateEngine;
 
-    @RabbitListener(queues="mail.welcome")
+    @RabbitListener(queues= MailConstants.MAIL_QUEUE_NAME)
     public void handler(Employee employee){
         MimeMessage msg = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(msg);
