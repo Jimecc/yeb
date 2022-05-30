b0d9ea27febe05a5dd46800f053c9ca4bff19ee9
Jimecc <1813085460@qq.com>
2022-05-31 04:06:43 +0800

    Update JobLeven

--- vue/src/views/sys/SysBasic.vue
+++ vue/src/views/sys/SysBasic.vue
@@ -3,7 +3,7 @@
   <el-tabs v-model="activeName" type="card">
     <el-tab-pane label="部门管理" name="DepMana"><DepMana></DepMana></el-tab-pane>
     <el-tab-pane label="职位管理" name="PosMana"><PosMana></PosMana></el-tab-pane>
-    <el-tab-pane label="职称管理" name="JoblevelMana"><JoblebelMana></JoblebelMana></el-tab-pane>
+    <el-tab-pane label="职称管理" name="JoblebelMana"><JoblebelMana></JoblebelMana></el-tab-pane>
     <el-tab-pane label="奖惩规则" name="EcMana"><EcMana></EcMana></el-tab-pane>
     <el-tab-pane label="权限组" name="PermissMana"><PermissMana></PermissMana></el-tab-pane>
   </el-tabs>
@@ -20,7 +20,7 @@ export default {
   name: "SysBasic",
   data(){
     return{
-      activeName:'PosMana'
+      activeName:'JoblebelMana'
     }
   },
   components:{
